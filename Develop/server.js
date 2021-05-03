a2ae5284de0a48ce0c67c29187951c7652912f5e
Zack <ahmedsakaria32@gmail.com>
2021-05-03 08:55:08 -0500

    setting up a public folder and adding the first route

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const express = require("express");
-
-const app = express();
 
 PORT = process.env.PORT || 3007;
+
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static folder for js,html,images,style.css
+app.use(express.static("public"));
+
+//Setting up the routes
+app.get("/addnotes",(req, res) => {
+    res.sendFile(path.join(__dirname,"../public/addnotes.html"))
+});
 
 
 
